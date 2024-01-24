3a3fde5a709286ce0093717221d492088fe86739
samkou <samkou@gmail.com>
2024-01-24 17:06:34 +0000

    new reqs

--- Hello.py
+++ Hello.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+!pip install yfinance
 import yfinance as yf
 import yahoo_fin.stock_info as si
 import pandas as pd
